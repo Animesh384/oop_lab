7fd63040cba0fb71e16fae5fc32c8970d5777dfa
akanoob <animeshk384@gmail.com>
2022-08-25 23:07:54 +0530

    oop

--- day-3/q5.cpp
+++ day-3/q5.cpp
@@ -16,24 +16,23 @@ class dis{
     }
     void output(){
         cout<<"feet:"<<feet;
-        cout<<"inch:"<<inch<<endl;
+        cout<<"inch:"<<inch;
     }
-    void sum(dis &ob1,dis &obj2){
-        int sum1=ob1.inch+obj2.inch;
-        int sum2=ob1.feet+obj2.inch;
+    void sum(dis &ob1,dis &ob2){
+        int sum1=ob1.inch+ob2.inch;
+        int sum2=ob1.feet+ob2.feet;
         while(sum1>=12){
             sum2++;
             sum1=sum1-12;
         }
-        cout<<"feet:"<<sum2<<endl;
-        cout<<"inch:"<<sum1<<endl;
+        cout<<"feet:"<<sum2<<" inch:"<<sum1<<endl;
     }
-    
-
 };
 int main(){
     dis ob1,ob2,ob3;
     ob1.input();
     ob2.input();
+    //ob1.output();
+    //ob2.output();
     ob3.sum(ob1,ob2);
 }
--- day-3/q6.cpp
+++ day-3/q6.cpp
@@ -3,20 +3,31 @@
 using namespace std;
 class emp{
     int id;
-    char name[10];
+    string name;
     int age;
     double bs;
     public:
-    void input(){
+    emp(){
         cout<<"enter a id:";
         cin>>id;
         cout<<"enter name:";
         cin>>name;
         cout<<"entr a  age:";
+        cin>>age;
+        cout<<"enter a base salary:";
+        cin>>bs;
     }
+    void display(){
+        double gs=bs+bs*0.8+bs*0.2;
+        printf("%d\t%s\t%d\t%.2f\t%.2f\n",id,name,age,bs,gs);
+    }
+
 };
 int main(){
  emp ob1[6];
+ cout<<"id\tname\tage\tbase\tgros"<<endl;
  for(int i=0;i<6;i++){
+    (ob1+i)->display();
  }
+ 
 }
