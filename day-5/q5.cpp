451ddfabffcf7b47136cfd797ca1b25e7c8840cd
akanoob <animeshk384@gmail.com>
2022-09-13 22:24:53 +0530

    oops

--- day-5/q5.cpp
+++ day-5/q5.cpp
@@ -1,35 +1,52 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class book{
+class book
+{
+    string b_name;
     string b_name;
     string auth;
     float price;
-    public:
+
+public:
     book();
-    friend void display(book*,int);
+    void set();
+    friend void display(book *, int);
 };
-book::book(){
-    cout<<"b_name:";
-    cin>>b_name;
-    cout<<"auth:";
-    cin>>auth;
-    cout<<"price:";
-    cin>>price;
-    //std::__cxx11::string book::b_name
+book::book()
+{
+    cout << "b_name:";
+    cin >> b_name;
+    cout << "auth:";
+    cin >> auth;
+    cout << "price:";
+    cin >> price;
+    // std::__cxx11::string book::b_name
 }
-void display(book* book,int n){
-    for(int i=0;i<n;i++){
-    cout<<"b_name:"<<book->b_name<<endl;
-    cout<<"auth:"<<book->auth<<endl;
-    cout<<"price:"<<book->price<<endl;
-    book++;
+void book::set(){
+    cout << "b_name:";
+    cin >> b_name;
+    cout << "auth:";
+    cin >> auth;
+    cout << "price:";
+    cin >> price;
+}
+void display(book *book, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << "b_name:" << book->b_name << endl;
+        cout << "auth:" << book->auth << endl;
+        cout << "price:" << book->price << endl;
+        book++;
     }
 }
-int main(){
-    cout<<"Enter a number of books:";
+int main()
+{
+    cout << "Enter a number of books:";
     int n;
-    cin>>n;
-    book* ob=new book[n];
-    display(ob,n);
+    cin >> n;
+    book *ob = new book[n];
+    display(ob, n);
+    book ob1=*ob;
     return 0;
 }
